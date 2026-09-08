d53d9fe24d045c3d1dcda138f5e38bdf6af17e25
Chr <luticr@gmail.com>
2026-09-08 13:48:48 -0400

    create tests

---
+++ src/test/java/tests/Test01.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Test01 {
+
+    @Test
+    public void test01() {
+        Assertions.assertEquals(5, 5);
+    }
+}
---
+++ src/test/java/tests/Test02.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Test02 {
+
+    @Test
+    public void test02() {
+        Assertions.assertEquals(5, 5);
+    }
+}
